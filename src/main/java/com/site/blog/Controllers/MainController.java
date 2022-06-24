880ba850d23100af945189da3e5cf9905b9da976
Kirill-Tretiakov <kirill.tretyakoff@gmail.com>
2022-06-24 12:50:11 +0300

    New get mapping method was added

---
+++ src/main/java/com/site/blog/Controllers/MainController.java
@@ -0,0 +1,20 @@
+package com.site.blog.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    public String mainPage(Model model){
+        model.addAttribute("title", "Home page");
+        return "main";
+    }
+    @RequestMapping(method = RequestMethod.GET, value = "/about")
+    public String aboutPage(Model model){
+        model.addAttribute("title", "About-us");
+        return "about";
+    }
+}
